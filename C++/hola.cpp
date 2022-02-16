bb53fbfde83ac0719ef072b674823963a6fda4d1
Lscys <porta2910@gmail.com>
2022-02-16 08:50:07 -0500

    primer viaje

---
+++ C++/hola.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+char nombre[20];
+int main()
+{
+    cout<<"Escriba su nombre: ";
+    cin>>nombre;
+    cout<<"Su nombres es: "<<nombre<<"\n";   
+    return 0;
+}
