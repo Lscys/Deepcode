f1fd72d62b21c4fa75e159282df09c18936ea283
Lscys <porta2910@gmail.com>
2022-02-16 11:13:57 -0500

    septimo viaje

--- C++/operadores.cpp
+++ C++/operadores.cpp
@@ -2,16 +2,16 @@
 #include<conio.h>
 using namespace std;
 float monto, porcentaje=0.18, igv, total;
-void Banner1(){
+void Banner(){
     cout<<"Este es mi segundo gran proyecto \n";
 }
 int main(){
-    Banner1();
+    Banner();
     cout<<"Ingrese su monto: ";
     cin>>monto;
-    igv =monto*porcentaje;
-    total =monto+igv;
-    cout<<"Su IGV es: "<<igv<<"\n";
+    igv=monto*porcentaje;
+    total=monto+igv;
+    cout<<"Su igv es: "<<igv<<"\n";
     cout<<"Su monto es: "<<total;
     return 0;
 }
