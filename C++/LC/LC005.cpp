fa314406982d44e52c26025642740dcc87967477
Lscys <porta2910@gmail.com>
2022-02-17 08:48:24 -0500

    LC005

---
+++ C++/LC/LC005.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int Suma, Num;
+int main(){
+	cout<<"Ingreso de numero entero: ";
+	cin>>Num;
+	Suma=(Num*(Num+1))/2;
+	cout<<"La suma es: "<<Suma;
+	return 0;
+}
