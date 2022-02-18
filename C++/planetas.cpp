8d6c33b37508a81dbd3dd6408c992b09bddafe50
Lscys <porta2910@gmail.com>
2022-02-17 19:42:28 -0500

    planetas.cpp

---
+++ C++/planetas.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+string Planeta[9]{"Mercurio", "Venus", "Tierra", "Marte", "Jupiter", "Saturno", "Urano", "Neptuno", "Pluton"};
+int Num;
+int main (){
+    cout<<"Ingrese en numero del planeta: ";
+    cin>>Num;
+    if(Num>=1&&Num<10){
+        cout<<"El planeta que eligio es: "<<Planeta[Num-1];
+    }else{
+        cout<<"Fuera de alcanze";
+    }
+    return 0;
+}
