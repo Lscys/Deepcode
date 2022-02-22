983d2f9ac6cfe7334fa4c8e12ac18fef15169ac7
Lscys <porta2910@gmail.com>
2022-02-22 17:15:41 -0500

    factura.cpp

--- C++/factura.cpp
+++ C++/factura.cpp
@@ -4,12 +4,9 @@
 #include<string.h>
 #include<fstream>
 using namespace std;
-void Banner(){
-    cout<<"En caso de ir a comprar a tienda, \n se espera el deposito de S/.168.66 a cuenta 191-27621568-0-72 ahorro \n soles BCP o 292-3056979545 ahorro soles INTERBANK y mandar sms de confirmacion 987412813"<<endl;
-}
 int main(){
-    float pag;
-    int Pm, CPU, RAM, DD, TV, Case, Monitor, UO, TM, Estabilizador, precio_final; 
+    float pag, IGV, precio_pagar;
+    int Pm, CPU, RAM, DD, TV, Case, Monitor, UO, TM, Estabilizador, precio_final, otros_final; 
     system("cls");
     cout<<"tipo de cambio de pagina: ";
     cin>>pag;
@@ -43,26 +40,32 @@ int main(){
     Monitor*=3.7;
     UO*=3.7;
     TM*=3.7;
+    otros_final=UO+TM+Estabilizador;
+    IGV=0.18;
     Estabilizador*=pag;
     precio_final=(Pm+CPU+RAM+DD+TV+Case+Monitor+UO+TM+Estabilizador);
+    precio_pagar=precio_final+IGV;
     cout<<"El precio en soles sin IGV es: "<<precio_final<<endl;
     ofstream archivo;
     archivo.open("factura.txt");
     archivo<<"=============================================="<<endl;
-    archivo<<"Factura / boleta"<<endl;
+    archivo<<"              Factura / boleta"<<endl;
     archivo<<"=============================================="<<endl;
-    archivo<<"Placa madre:______________________________"<<Pm<<endl;
-    archivo<<"Procesador CPU:___________________________"<<CPU<<endl;
-    archivo<<"Memoria RAM:______________________________"<<RAM<<endl;
-    archivo<<"Disco Duro:_______________________________"<<DD<<endl;
-    archivo<<"Tarjeta de video:_________________________"<<TV<<endl;
-    archivo<<"Case:_____________________________________"<<Case<<endl;
-    archivo<<"Monitor:__________________________________"<<Monitor<<endl;
-    archivo<<"Otros: "<<UO<<"+"<<TM<<"+"<<Estabilizador<<endl;
-    archivo<<"El precio en soles sin IGV es: "<<precio_final<<endl;
+    archivo<<"Placa madre:______________________________S/."<<Pm<<endl;
+    archivo<<"Procesador CPU:___________________________S/."<<CPU<<endl;
+    archivo<<"Memoria RAM:______________________________S/."<<RAM<<endl;
+    archivo<<"Disco Duro:_______________________________S/."<<DD<<endl;
+    archivo<<"Tarjeta de video:_________________________S/."<<TV<<endl;
+    archivo<<"Case:_____________________________________S/."<<Case<<endl;
+    archivo<<"Monitor:__________________________________S/."<<Monitor<<endl;
+    archivo<<"Otros:____________________________________S/."<<otros_final<<endl;
+    archivo<<endl;
+    archivo<<"El precio en soles sin IGV es:___________S/."<<precio_final<<endl;
+    archivo<<"IGV:_____________________________________S/."<<IGV<<endl;
+    archivo<<"Total a pagar:___________________________S/."<<precio_pagar<<endl;
     archivo<<"\n En caso de ir a comprar a tienda, se espera el deposito \n de S/.168.66 a cuenta 191-27621568-0-72 ahorro \n soles BCP o 292-3056979545 ahorro soles INTERBANK y mandar sms de \n confirmacion 987412813"<<endl;
     archivo.close();
-    getch();
     system("Start factura.txt");
+    getch();
     return 0;
 }
