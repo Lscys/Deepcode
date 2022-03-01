3269bc6cd3f381e9f3817a99b883f4c4fb4bef40
Lscys <porta2910@gmail.com>
2022-03-01 15:35:11 -0500

    practica2.py

---
+++ Python/practica1.py
@@ -0,0 +1,13 @@
+# numero1 = int(input('Escribe un numero porfavor: '))
+# numero2 = int(input('Escribe un numero porfavor: '))
+# numero3 = int(input('Escribe un numero porfavor: '))
+# print('Mayor ', max(numero1, numero2, numero3))
+# print('Menor ', min(numero1, numero2, numero3))
+
+
+numero1 = int(input('Escribe un numero porfavor: '))
+numero2 = int(input('Escribe un numero porfavor: '))
+numero3 = int(input('Escribe un numero porfavor: '))
+print('El numero mayor entre {}, {} y {} es: {}, y el menor es {}'.format(numero1, numero2, numero3,
+    max(numero1, numero2, numero3),
+    min(numero1, numero2, numero3)))
---
+++ Python/practica2.py
@@ -0,0 +1,12 @@
+# grados = input('Ingrese los grados fahrenheit: ')
+# grados = float(grados)
+# celsius =(grados - 32)*5/9
+# celsius = round(celsius, 2)
+# print('Su grados Celsius es: '+str(celsius))
+
+
+libra = int(input('Cuantas libras quieres: '))
+libra = float(libra)
+valor_euro=1.20
+euro = (libra/valor_euro)
+print('Le costara €,'+str(euro))
