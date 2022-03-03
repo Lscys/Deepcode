84e4c2b6c1f54459d50b4e31fce7a76171002b20
Lscys <porta2910@gmail.com>
2022-03-03 10:33:28 -0500

    icono

---
+++ Python/practica3.py
@@ -0,0 +1,14 @@
+
+numero = int(input('Escoge una numero del 1 al 10: '))
+numero_elegido = 6
+
+if numero >= numero_elegido:
+    print('El numero es muy grande, escoge uno mas pequeño')
+    
+if numero <= numero_elegido:
+    print('El numero es muy chico, escoge uno mas grande')
+    
+if numero == numero_elegido:
+    print("En Hora buena, ganaste")
+
+
