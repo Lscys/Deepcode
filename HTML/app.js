afe934dc82cc9994b81b0542202fff1d48ffa04f
Lscys <porta2910@gmail.com>
2022-03-07 11:47:43 -0500

    HTML

---
+++ HTML/app.js
@@ -0,0 +1,15 @@
+
+function texto(){
+    let text = document.getElementById("demo1").value;
+    document.getElementById("demo").innerHTML = text;
+}
+
+function car(){
+	let text = document.getElementById("cars").value;
+	document.getElementById("selecionado").innerHTML = text;
+}
+
+function text_area(){
+	let text = document.getElementById("text_area").value;
+	document.getElementById("text_area1").innerHTML = text;
+}
